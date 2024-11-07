48339c46a7ed2cc091ef4adb235b2f6c856d200e
Egor00f <velikiydolbayeb@gmail.com>
2024-11-07 21:09:03 +0500

    hmmm... wget with -q flag fall...

--- install.bat
+++ install.bat
@@ -30,7 +30,7 @@ Call :print_msg Downloading libraries...
 
 cd %TOOLCHAIN_DIR%\win32\mingw32
 
-wget http://ftp.kolibrios.org/users/Serge/new/Toolchain/sdk-28-10-16.7z -q -O %TOOLCHAIN_DIR%\win32\mingw32\sdk-28-10-16.7z
+wget http://ftp.kolibrios.org/users/Serge/new/Toolchain/sdk-28-10-16.7z -O %TOOLCHAIN_DIR%\win32\mingw32\sdk-28-10-16.7z
 
 7z x -y %TOOLCHAIN_DIR%\win32\mingw32\sdk-28-10-16.7z
 
@@ -42,15 +42,15 @@ Call :print_msg Updating libraries
 
 cd %TOOLCHAIN_DIR%\win32\mingw32\lib
 
-wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libdll.a -q -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libdll.a
-wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libfreetype.a -q -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libfreetype.a
-wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libc.dll.a -q -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libc.dll.a
-wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libSDLn.a -q -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libSDLn.a
-wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libcurses.a -q -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libcurses.a 
-wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libz.dll.a -q -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libz.dll.a
-wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libogg.a -q -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libogg.a
-wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libvorbis.a -q -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libvorbis.a
-wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libopenjpeg.a -q -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libopenjpeg.a
+wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libdll.a -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libdll.a
+wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libfreetype.a -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libfreetype.a
+wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libc.dll.a -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libc.dll.a
+wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libSDLn.a -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libSDLn.a
+wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libcurses.a -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libcurses.a 
+wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libz.dll.a -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libz.dll.a
+wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libogg.a -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libogg.a
+wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libvorbis.a -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libvorbis.a
+wget http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/libopenjpeg.a -O %TOOLCHAIN_DIR%\win32\mingw32\lib\libopenjpeg.a
 
 Call :print_ok Successfully!
 
